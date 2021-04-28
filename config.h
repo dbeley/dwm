5bc3d4bfbaa44d094f683f3cdedcafc11d4ae372
dbeley <6568955+dbeley@users.noreply.github.com>
2021-04-28 18:54:01 +0200

    Update.

--- config.h
+++ config.h
@@ -8,17 +8,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Iosevka Nerd Font:style=Italic:pixelsize=14:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Iosevka Nerd Font:style=Italic:pixelsize=14:antialias=true:autohint=true";
-/*#include "/home/david/.cache/wal/colors-wal-dwm.h" */
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
+#include "/home/david/.cache/wal/colors-wal-dwm.h"
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -58,7 +48,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *mutecmd[] = { "~/scripts/pamixer.sh", "mute", NULL };
 static const char *volupcmd[] = { "~/scripts/pamixer.sh", "up", NULL };
